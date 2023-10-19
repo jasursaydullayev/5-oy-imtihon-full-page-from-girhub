d23ab11acc1e9cb4be22d394ed8b0734fecf53af
Jasurbek1810 <JasurbekSaydullayev@gmail.com>
2023-10-19 12:18:17 +0500

    exam-full-pages

---
+++ js/request.js
@@ -0,0 +1,45 @@
+// API
+const API = `https://countries-api-v7sn.onrender.com/countries?limit=250`;
+
+// For Loader
+const loader = document.querySelector(".loader");
+const loaderToggle = (item) => {
+  if (item) {
+    loader.classList.remove("hidden");
+  } else {
+    loader.classList.add("hidden");
+  }
+};
+//  request promise
+const getData = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState != 4) {
+        loaderToggle(true);
+      } else if (request.readyState == 4 && request.status == 200) {
+        const result = JSON.parse(request.responseText);
+        resolve(result.data);
+        loaderToggle(false);
+      } else if (request.readyState == 4) {
+        loaderToggle(false);
+        reject(`Eror`);
+      }
+    });
+
+    request.open(`GET`, resource);
+    request.send();
+  });
+};
+// Funksiya
+const load = () => {
+  getData(API)
+    .then((data) => {
+      UpdateUI(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+document.addEventListener("DOMContentLoaded", load);
